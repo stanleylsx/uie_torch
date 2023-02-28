0227f93033e1861256d98aac0dd683fd9c9a474a
lishouxian <gzlishouxian@gmail.com>
2023-02-28 11:16:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,11 @@ configure = {
     'val_file': 'datasets/dev.txt',
     # 测试数据集
     'test_file': 'datasets/dev.txt',
+    # 引擎onnx或者pytorch
     'engine': 'pytorch',
+    # 模型语言
     'schema_lang': 'zh',
+    # 是否多语言
     'multilingual': False,
     # 没有验证集时，从训练集抽取验证集比例
     'validation_rate': 0.1,
@@ -34,11 +37,13 @@ configure = {
     'checkpoints_dir': 'checkpoint',
     # 句子最大长度
     'max_sequence_length': 512,
+    # 判决阈值
     'position_prob': 0.5,
     # 分割句子
     'split_sentence': True,
     # 随机种子
     'seed': 1000,
+    # 使用fp16
     'use_fp16': False,
     # 微调阶段的epoch
     'epoch': 30,

c3398864c7144436b78280c5d4bd9a4ace87d1b8
Stanley <9429671+stanleylsx@users.noreply.github.com>
2024-02-29 10:02:04 +0800

    PEP8规范

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def fold_check(configures):
 if __name__ == '__main__':
     set_seed(configure['seed'])
     fold_check(configure)
-    logger = Logger(name='UIE', log_dir=configure['checkpoints_dir'] + '/logs', mode=mode)
+    logger = Logger(name='UIE', log_dir=configure['checkpoints_dir'] + '/logs', mode=mode).logger
     if use_cuda:
         if torch.cuda.is_available():
             if cuda_device == -1:
@@ -45,11 +45,11 @@ if __name__ == '__main__':
     else:
         device = 'cpu'
 
-    logger.logger.info(f'device: {device}')
-    logger.logger.info(json.dumps(configure, indent=2, ensure_ascii=False))
+    logger.info(f'device: {device}')
+    logger.info(json.dumps(configure, indent=2, ensure_ascii=False))
     if mode == 'train':
         from engines.train import Train
-        logger.logger.info('mode: train')
+        logger.info('mode: train')
         train = Train(device, logger)
         train.train()
     elif mode == 'test':
@@ -61,16 +61,16 @@ if __name__ == '__main__':
         predict = Predict(device, logger)
         predict.predict_one('warm up')
         while True:
-            logger.logger.info('please input a sentence (enter [exit] to exit.)')
+            logger.info('please input a sentence (enter [exit] to exit.)')
             print('please input a sentence (enter [exit] to exit.)')
             sentence = input()
             if sentence == 'exit':
                 break
-            logger.logger.info('input:{}'.format(str(sentence)))
+            logger.info('input:{}'.format(str(sentence)))
             start_time = time.time()
             result = predict.predict_one(sentence)
             time_cost = (time.time() - start_time) * 1000
-            logger.logger.info('putput:{}, cost {}(ms).'.format(str(result), time_cost))
+            logger.info('putput:{}, cost {}(ms).'.format(str(result), time_cost))
             pprint(result)
             print('time consumption: %.3f(ms)' % time_cost)
     elif mode == 'export_torch':
